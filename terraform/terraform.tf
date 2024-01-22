8fdb1fd5ebf17ef8c699a2922dd44afaebe2639e
Aboelkhair93 <ahmed.aboelkhair14@gmail.com>
2024-01-22 14:53:09 +0200

    a

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,7 +27,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "vprofileactions92"
+    bucket = "vprofileactions93"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-12"
 }
 
 variable "clusterName" {
